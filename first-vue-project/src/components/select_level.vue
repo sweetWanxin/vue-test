9bf8c5650a81b2c9af1af58b24cc81dd08faf062
xiang <1278398904@qq.com>
2018-06-17 02:37:12 +0800

    vuex-cart

---
+++ first-vue-project/src/components/select_level.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text" v-model="content">
+  </div>
+</template>
+<script>
+  export default{
+    data(){
+      return {
+      }
+    },
+    computed:{
+      content(){
+        return this.$store.state.cart.name
+      }
+    }
+  }
+</script>
+<style>
+
+</style>
---
+++ first-vue-project/src/store/modules/cart.js
@@ -0,0 +1,11 @@
+/**
+ * Created by wwj on 2018/6/17.
+ */
+
+const state={
+  name:'陈翔'
+}
+
+export default{
+  state
+}
